e4800e8c1c4f77ec608ff95e1a02f47f836ec7be
nyavaidya <vaidyaabhidnya@gmail.com>
2026-03-17 11:03:54 +0530

    v3

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,5 +1,5 @@
-const supabaseUrl = "YOUR_SUPABASE_URL"
-const supabaseKey = "YOUR_SUPABASE_ANON_KEY"
+const supabaseUrl = "https://mqvcamtirxgupzcdlncx.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xdmNhbXRpcnhndXB6Y2RsbmN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2NjU2MjMsImV4cCI6MjA4OTI0MTYyM30.LXgpqplMAElmEiE9yn-Msai2rz4UbWIs0UR-K1T2xFs"
 
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
 
